7736abbcba4422fd1ef19925276b0c3f8fd8b315
Jang Min Jong <45162639+mjJang94@users.noreply.github.com>
2020-11-13 18:12:09 +0900

    Create 백준_좌표정렬하기_11650.java

---
+++ 단계별로_풀기/백준_좌표정렬하기_11650.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
+        int[][] arr = new int[N][2];
+
+
+        for (int i = 0; i < N; i++){
+
+            arr[i][0] = scan.nextInt();
+            arr[i][1] = scan.nextInt();
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]){
+                    return o1[1] - o2[1];
+                } else {
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+
+    }
+}
