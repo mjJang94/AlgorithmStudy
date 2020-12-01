a43739af58e8f7420b9dab7415dfa732c4794165
Jang Min Jong <alswhddl10@naver.com>
2020-12-01 13:48:02 +0900

    Create 백준_A+B-8.java

---
+++ 단계별로_풀기/백준_A+B-8.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+
+        for (int i = 0; i < N ; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+            String left = stringTokenizer.nextToken();
+            String right = stringTokenizer.nextToken();
+
+            stringBuilder
+                    .append("Case #" + (i+1) +": " + left + " + " + right + " = ")
+                    .append(Integer.parseInt(left) + Integer.parseInt(right))
+                    .append('\n');
+        }
+
+        bufferedReader.close();
+
+        System.out.println(stringBuilder);
+    }
+}
