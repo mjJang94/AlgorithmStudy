d2cda3089401177e998d3491ad13b6153760c0cf
Jang Min Jong <alswhddl10@naver.com>
2020-12-01 13:44:29 +0900

    Create 백준_N찍기_2741.java

---
+++ 단계별로_풀기/백준_N찍기_2741.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+
+        for (int i = 1; i < N + 1; i++) {
+
+            stringBuilder.append(i).append('\n');
+        }
+
+        bufferedReader.close();
+
+        System.out.println(stringBuilder);
+    }
+}
