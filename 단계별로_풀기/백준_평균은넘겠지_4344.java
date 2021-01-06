fbf6d7dd5d0777297fc54487a0190effd077486b
Jang Min Jong <alswhddl10@naver.com>
2021-01-06 23:22:03 +0900

    Create 백준_평균은넘겠지_4344.java

---
+++ 단계별로_풀기/백준_평균은넘겠지_4344.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(reader.readLine());
+
+        String[] arr = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = reader.readLine();
+        }
+
+        reader.close();
+
+        for (String s : arr) {
+
+            StringTokenizer st = new StringTokenizer(s," ");
+
+            int total = Integer.parseInt(st.nextToken());
+            double sum = 0;
+            int[] scores = new int[total];
+            int index = 0;
+
+            while (st.hasMoreTokens()) {
+                int tmpScore = Integer.parseInt(st.nextToken());
+                scores[index] = tmpScore;
+                sum += tmpScore;
+                index++;
+            }
+
+            double avg = sum / total;
+            double overAvg = 0;
+
+            for (int score : scores) {
+                if (score > avg) {
+                    overAvg++;
+                }
+            }
+              System.out.printf("%.3f%%\n", (overAvg/total) * 100);
+        }
+    }
+}
