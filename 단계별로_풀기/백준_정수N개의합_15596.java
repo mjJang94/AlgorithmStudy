76c1d83af439f5cb96e3862fd9d2f385e5e51efc
Jang Min Jong <alswhddl10@naver.com>
2021-01-08 12:02:09 +0900

    Create 백준_정수N개의합_15596.java

---
+++ 단계별로_풀기/백준_정수N개의합_15596.java
@@ -0,0 +1,12 @@
+
+//엄청 간단하지만 문제를 잘읽어보고 요구사항에 집중하자
+
+class Test {
+     long sum(int[] a) {
+        long sum = 0;
+        for (int tmp : a) {
+            sum += tmp;
+        }
+        return sum;
+    }
+}
