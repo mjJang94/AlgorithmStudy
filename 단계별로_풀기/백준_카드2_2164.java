3e55ae76527712a003233d4c457a6b4369990bd0
Jang Min Jong <alswhddl10@naver.com>
2021-01-10 21:25:11 +0900

    Update 백준_카드2_2164.java

--- 단계별로_풀기/백준_카드2_2164.java
+++ 단계별로_풀기/백준_카드2_2164.java
@@ -1,4 +1,7 @@
-public class Main{
+import java.io.*;
+import java.util.*;
+
+public class Main {
 
 //요구사항
 //N장의 카드가 있다. 각각의 카드는 차례로 1부터 N까지의 번호가 붙어 있으며, 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다.
@@ -6,8 +9,12 @@ public class Main{
 //예를 들어 N=4인 경우를 생각해 보자. 카드는 제일 위에서부터 1234 의 순서로 놓여있다. 1을 버리면 234가 남는다. 여기서 2를 제일 아래로 옮기면 342가 된다. 3을 버리면 42가 되고, 4를 밑으로 옮기면 24가 된다. 마지막으로 2를 버리고 나면, 남는 카드는 4가 된다.
 //N이 주어졌을 때, 제일 마지막에 남게 되는 카드를 구하는 프로그램을 작성하시오.
 
-  public static void Main(args[]){
-  
 
-  }
+    public static void main(String[] args){
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(reader.readLine());
+
+    }
 }
