5ce0eb2353ebc4bcffcf37bebf3797240540616f
mjJang94 <alswhddl10@naver.com>
2021-02-14 18:11:14 +0900

    Update 백준_N과M_15649.java

--- 단계별로_풀기/백준_N과M_15649.java
+++ 단계별로_풀기/백준_N과M_15649.java
@@ -24,19 +24,19 @@ public class Main {
 
     public static void dfs(int depth) {
 
-        if (depth == M){ //최대수인 4까지 탐색한 경
-            for (int tmp: arr){ //배열에 있는 데이터들을 꺼내서 출력한다.
+        if (depth == M){ 
+            for (int tmp: arr){
                 buffer.append(tmp).append(' ');
             }
-            buffer.append('\n'); //줄바꿈
-            return; //for문으로 돌아가기
+            buffer.append('\n'); 
+            return; 
         }
 
         for (int i=0; i< N; i++){
-            if (!visit[i]){ //탐색하지 않은 경우이면 조건문 실행
-                visit[i] = true; //false -> true로 바꿔줌
-                arr[depth] = i + 1; //데이터 담기
-                dfs(depth + 1); //재귀 호출
+            if (!visit[i]){ 
+                visit[i] = true;
+                arr[depth] = i + 1; 
+                dfs(depth + 1);
                 visit[i] = false;
             }
         }
